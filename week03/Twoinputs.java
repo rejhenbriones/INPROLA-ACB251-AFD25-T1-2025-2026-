0674d282dba7b15a4aa31cffe4c435d7a5dc908b
Rejhen Briones <rrbriones@student.apc.edu.ph>
2025-08-15 21:37:17 +0800

    commit

---
+++ week03/Twoinputs.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);
+
+    System.out.println("Enter two integers");
+
+     // Numerical input
+    int first number = myObj.nextInt();
+    int second number = myObj.nextInt();
+    
+
+    // Output input by user
+    System.out.println("first number " + first number); 
+    System.out.println("second number " + second number); 
+   
+  }
+}
